35e67f1880a961a0825c529c8757648ec7d28111
Adam Lim <aedamjung@gmail.com>
2023-08-27 19:18:36 +0800

    about.js

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutPage = () => {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to our about page! This is where you can learn more about our company.</p>
+    </div>
+  );
+};
+
+export default AboutPage;
