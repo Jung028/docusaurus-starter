4067e03903de4d2121b1ff248102d2c44e4608c1
Adam Lim <aedamjung@gmail.com>
2023-08-27 19:28:42 +0800

    about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,53 @@
 import React from 'react';
+import clsx from 'clsx';
+import styles from './styles.module.css';
 
-const AboutPage = () => {
+const AboutList = [
+  {
+    title: 'Our Mission',
+    Svg: require('@site/static/img/undraw_mission.svg').default,
+    description: (
+      <>
+        We are dedicated to providing cutting-edge solutions for automated marking in the education industry.
+      </>
+    ),
+  },
+  {
+    title: 'Our Team',
+    Svg: require('@site/static/img/undraw_team.svg').default,
+    description: (
+      <>
+        Our team consists of experienced professionals who are passionate about revolutionizing education technology.
+      </>
+    ),
+  },
+  // Add more sections as needed
+];
+
+function AboutSection({Svg, title, description}) {
   return (
-    <div>
-      <h1>About Us</h1>
-      <p>Welcome to our about page! This is where you can learn more about our company.</p>
+    <div className={clsx('col col--4')}>
+      <div className="text--center">
+        <Svg className={styles.featureSvg} role="img" />
+      </div>
+      <div className="text--center padding-horiz--md">
+        <h3>{title}</h3>
+        <p>{description}</p>
+      </div>
     </div>
   );
-};
+}
 
-export default AboutPage;
+export default function About() {
+  return (
+    <section className={styles.about}>
+      <div className="container">
+        <div className="row">
+          {AboutList.map((props, idx) => (
+            <AboutSection key={idx} {...props} />
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+}
